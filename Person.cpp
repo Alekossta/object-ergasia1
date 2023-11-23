d59f6d2cc56bc37123ac7a923bb770c239b10d61
Alekossta <alekosstavrinoudakis@gmail.com>
2023-11-23 11:13:00 +0200

    first commit

---
+++ Person.cpp
@@ -0,0 +1,41 @@
+#include "Person.h"
+#include <iostream>
+#include <string>
+
+Person::Person(): name(""), age(0) {
+    count++;
+}
+
+Person::Person(const std::string& startingName): name(startingName), age(0) {
+    count++;
+}
+
+Person::Person(int startingAge): name(""), age(startingAge) {
+    count++;
+}
+
+Person::Person(int startingAge, const std::string& startingName) : name(startingName), age(startingAge)
+{
+    count++;
+}
+
+Person::~Person() {
+    count--;
+}
+
+int Person::getCount() {
+    return count;
+}
+
+std::istream& operator>>(std::istream& input, Person& person) {
+    std::cout << "Enter name: ";
+    input >> person.name;
+    std::cout << "Enter age: ";
+    input >> person.age;
+    return input;
+}
+
+std::ostream& operator<<(std::ostream& output, const Person& person) {
+    output << "Name: " << person.name << ", Age: " << person.age;
+    return output;
+}
---
+++ Person.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Person
+{
+    static int count; // count how many people we have
+    std::string name;
+    unsigned age;
+
+    public:
+        // constructors
+        Person();
+        Person(const std::string& startingName);
+        Person(int startingAge);
+        Person(int startingAge, const std::string& startingName);
+        
+        // deconstructors
+        ~Person();
+
+        static int getCount() {return count; };
+
+        friend std::istream& operator>>(std::istream& input, Person& person);
+
+        friend std::ostream& operator<<(std::ostream& output, const Person& person);
+
+}   
